65bb0ba5b6bb428736f82f1be4e6f43580b894e0
zgeen3 <zgeen3@gmail.com>
2018-11-19 14:07:08 +0300

    Commit # 9 (api-gateway addendum # 2)

--- api-gateway/src/main/java/smirnov/bn/apigateway/controller/ApiGatewayController.java
+++ api-gateway/src/main/java/smirnov/bn/apigateway/controller/ApiGatewayController.java
@@ -2,25 +2,39 @@ package smirnov.bn.apigateway.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.web.util.UriComponentsBuilder;
 import smirnov.bn.apigateway.info_model_patterns.LingVarInfo;
 import smirnov.bn.apigateway.info_model_patterns.EmployeeInfo;
 import smirnov.bn.apigateway.info_model_patterns.LingVarWithEmployeeInfo;
 
+import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
 @RestController
 @RequestMapping("/gateway_API")
 public class ApiGatewayController {
+
+    // https://stackoverflow.com/questions/30528255/how-to-access-a-value-defined-in-the-application-properties-file-in-spring-boot (:)
+    @Value("${server.port}")
+    private int port;
+    private String server = "localhost";
 
     //https://stackoverflow.com/questions/14432167/make-a-rest-url-call-to-another-service-by-filling-the-details-from-the-form
     //@Autowired
@@ -71,22 +85,44 @@ public class ApiGatewayController {
         }
     }
 
+    @PostMapping("/create-ling_var")
+    public ResponseEntity<String> createLingVar(HttpServletRequest request, @RequestBody LingVarInfo lingVarInfo)
+            throws URISyntaxException {
+        return this.proxingExternalRequests(lingVarInfo.toString(), HttpMethod.POST, request);
+    }
+
     //http://localhost:8193/employees/create-employee
-    /*  //https://stackoverflow.com/questions/14726082/spring-mvc-rest-service-redirect-forward-proxy (:)
-        private String server = "localhost";
-        private int port = 8080;
+    ///*
+    //https://stackoverflow.com/questions/14726082/spring-mvc-rest-service-redirect-forward-proxy (:)
+    //@RequestMapping("/**")
+    @RequestMapping(value = {"/ling_var_dict/**", "/biz_proc_desc/**", "/employees/**"}, method = {GET, POST, PUT, DELETE})
+    @ResponseBody
+    public ResponseEntity<String> proxingExternalRequests(@RequestBody(required = false) String body,
+                                                  HttpMethod method, HttpServletRequest request) //, HttpServletResponse response)
+            throws URISyntaxException {
+        URI uri = new URI("http", null, server, port, null, null, null);
+        uri = UriComponentsBuilder.fromUri(uri)
+                .path(request.getRequestURI())
+                .query(request.getQueryString())
+                .build(true).toUri();
 
-        @RequestMapping("/**")
-        @ResponseBody
-        public String mirrorRest(@RequestBody String body, HttpMethod method, HttpServletRequest request) throws URISyntaxException
-        {
-            URI uri = new URI("http", null, server, port, request.getRequestURI(), request.getQueryString(), null);
-
-            ResponseEntity<String> responseEntity =
-                restTemplate.exchange(uri, method, new HttpEntity<String>(body), String.class);
-
-            return responseEntity.getBody();
+        HttpHeaders headers = new HttpHeaders();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            headers.set(headerName, request.getHeader(headerName));
         }
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            return restTemplate.exchange(uri, method, httpEntity, String.class);
+        } catch (HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getRawStatusCode())
+                    .headers(e.getResponseHeaders())
+                    .body(e.getResponseBodyAsString());
+        }
+    }
     //*/
 
 
