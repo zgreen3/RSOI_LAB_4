9fe0b88cde78a6c9b5c9c399681e79fdaaf57866
Boris Smirnov <zgreen3@gmail.com>
2019-01-11 03:39:10 +0300

    Laboratory work # 3 after New Year commit # 8

--- web_spring_app_1/src/main/java/smirnov/bn/web_spring_app_1/controller/MainController.java
+++ web_spring_app_1/src/main/java/smirnov/bn/web_spring_app_1/controller/MainController.java
@@ -2,10 +2,7 @@ package smirnov.bn.web_spring_app_1.controller;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -90,16 +87,19 @@ public class MainController {
     //[Spring MVC will give you the HttpRequest if you just add it to your controller method signature]
     //[https://stackoverflow.com/questions/8504258/spring-3-mvc-accessing-httprequest-from-controller] (:)
     //[https://stackoverflow.com/questions/40899494/how-to-get-input-values-from-spring-boot-thyme-leaf-to-java-class](:)
-    @RequestMapping(value = {"/employee-{UUID}"}, method = RequestMethod.GET) //, produces = "text/html;charset=UTF-8")
-    public String showEmployeePage(Model model, @PathVariable("UUID") UUID employeeUuid) throws URISyntaxException {
+    //https://stackoverflow.com/questions/17821731/spring-mvc-how-to-indicate-whether-a-path-variable-is-required-or-not (:)
+    //https://stackoverflow.com/questions/4904092/with-spring-3-0-can-i-make-an-optional-path-variable (:)
+    @RequestMapping(value = {"/employee/{employeeUUID}", "/employee", "/employee/"}, method = RequestMethod.GET)
+    //, produces = "text/html;charset=UTF-8")
+    public String showEmployeePage(Model model, @PathVariable("employeeUUID") Optional<UUID> employeeUuid) throws URISyntaxException {
         logger.info("MainController web_spring_app_1 showEmployeePage() request API_Gateway_controller - START");
         EmployeeForm employeeForm;
-        if (employeeUuid == null) {
+        if (!employeeUuid.isPresent()) {
             employeeForm = new EmployeeForm();
             //employeeCreating (:)
             model.addAttribute("employeeEditing", false);
         } else {
-            EmployeeInfo newEmployeeInfo = service.findEmployeeByUuid(employeeUuid);
+            EmployeeInfo newEmployeeInfo = service.findEmployeeByUuid(employeeUuid.get());
             employeeForm = new EmployeeForm(newEmployeeInfo.getEmployeeId(), newEmployeeInfo.getEmployeeName(),
                     newEmployeeInfo.getEmployeeEmail(), newEmployeeInfo.getEmployeeLogin(), newEmployeeInfo.getEmployeeUuid());
             model.addAttribute("employeeEditing", true);
@@ -113,11 +113,11 @@ public class MainController {
     //using Spring's ModelAttribute method parameter]
     //[https://stackoverflow.com/questions/8504258/spring-3-mvc-accessing-httprequest-from-controller] (:)
     //[Spring MVC will give you the HttpRequest if you just add it to your controller method signature]
-    @RequestMapping(value = {"/employee-{UUID}"}, method = RequestMethod.POST)
+    @RequestMapping(value = {"/employee/{employeeUUID}", "/employee", "/employee/"}, method = RequestMethod.POST)
     public String saveEmployee(Model model,
                                @Valid @ModelAttribute("employeeForm") EmployeeForm employeeForm,
                                BindingResult result,
-                               @PathVariable("UUID") UUID employeeUuid
+                               @PathVariable("employeeUUID") Optional<UUID> employeeUuid
     ) {
         logger.info("MainController web_spring_app_1 saveEmployee() request API_Gateway_controller - START");
         if (result.hasErrors()) {
@@ -133,11 +133,11 @@ public class MainController {
                 && employeeLogin != null && employeeLogin.length() > 0) {
             EmployeeInfo newEmployeeInfo = new EmployeeInfo(employeeName, employeeEmail, employeeLogin);
 
-            if (employeeUuid != null) {
-                newEmployeeInfo.setEmployeeUuid(employeeUuid);
+            if (employeeUuid.isPresent()) {
+                newEmployeeInfo.setEmployeeUuid(employeeUuid.get());
                 service.updateEmployee(newEmployeeInfo);
                 logger.info("MainController web_spring_app_1 saveEmployee() request API_Gateway_controller - employee being edited UUID: " +
-                        employeeUuid.toString());
+                        employeeUuid.get().toString());
             } else {
                 UUID newEmployeeUUID = service.createEmployee(newEmployeeInfo);
                 //employees.add(newEmployeeInfo);
@@ -160,11 +160,18 @@ public class MainController {
     //(+) https://stackoverflow.com/questions/44170932/attempting-to-make-a-delete-button-in-thyme-leaf-spring
     //(+) https://stackoverflow.com/questions/22794057/thymeleaf-send-parameter-from-html-to-controller
     //(+) https://stackoverflow.com/questions/43606063/spring-thymeleaf-delete-object-from-html-table-and-pass-id-to-controller?rq=1
-    @RequestMapping(value = {"/employee_delete_{employeeUuid}"}, method = RequestMethod.POST)
+    //https://javatalks.ru/topics/question/46293
+    //https://stackoverflow.com/questions/13629653/using-put-and-delete-methods-in-spring-mvc
+    //https://stackoverflow.com/questions/18418132/handling-delete-in-rest?noredirect=1&lq=1
+    //https://docs.spring.io/spring/docs/4.2.x/javadoc-api/org/springframework/web/filter/HiddenHttpMethodFilter.html
+    //https://stackoverflow.com/questions/24256051/delete-or-put-methods-in-thymeleaf
+    //https://www.w3schools.com/tags/att_input_type_hidden.asp
+    //https://stackoverflow.com/questions/38370011/thymeleaf-button-click-to-call-http-delete-method?noredirect=1&lq=1
+    @RequestMapping(value = {"/employee/{employeeUuid}/delete"}, method = RequestMethod.DELETE) //RequestMethod.DELETE) //RequestMethod.POST)
     //@PostMapping("/employee_delete_{employeeUuid}")
     public //@ResponseBody
-           String deleteEmployee(Model model,
-                               @PathVariable("employeeUuid") String employeeUuid
+    String deleteEmployee(Model model,
+                          @PathVariable("employeeUuid") String employeeUuid
     ) {
         logger.info("MainController web_spring_app_1 deleteEmployee() request API_Gateway_controller - START");
         service.deleteEmployeeByUuid(UUID.fromString(employeeUuid));
